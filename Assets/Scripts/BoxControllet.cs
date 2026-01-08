0c8884ef14b7bbce56ccf1e1aa91200e6c961ced
NoemiSossella <noemi.sossella@gmail.com>
2026-01-08 18:19:52 +0100

    correction

--- Assets/Scripts/BoxControllet.cs
+++ Assets/Scripts/BoxControllet.cs
@@ -2,41 +2,36 @@ using UnityEngine;
 using System.Collections;
 public class BoxControllet : MonoBehaviour
 {
-    public GameObject objectToDisable;
-
     private Rigidbody2D rb;
     private bool hasFallen = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.gravityScale = 0f; // Non cade all'inizio
+        rb.gravityScale = 0f; // fermo all'inizio
     }
 
-    // Click del mouse sull’oggetto
     void OnMouseDown()
     {
         if (!hasFallen)
         {
-            rb.gravityScale = 1f; // Inizia a cadere
+            rb.gravityScale = 1f; // inizia a cadere
             hasFallen = true;
         }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        // Se colpisce un oggetto con tag Killer
         if (collision.gameObject.CompareTag("Killer"))
         {
-            if (objectToDisable != null)
-            {
-                objectToDisable.SetActive(false);
-            }
+            // Disattiva L'OGGETTO COLPITO
+            collision.gameObject.SetActive(false);
 
-            // Ferma l’oggetto
+            // Ferma la caduta
             rb.linearVelocity = Vector2.zero;
             rb.gravityScale = 0f;
         }
     }
 }
+
 
